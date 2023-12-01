9a35dcad11d0a2c22e23b868fe9ebc2609aa08ce
ecee <you@example.com>
2023-12-01 22:13:02 +0300

    Done with Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', true),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', true),
+  ('Tabitha', 8, 'F', 'White', 'Calm', false),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Strict', true),
+  ('Melissa', 4, 'F', 'Golden', 'Energetic', true),
+  ('Grinch', 2, 'M', 'Green', 'Mischievous', false),
+  ('Wendy', 7, 'F', 'Black', 'Sweet', true),
+  (NULL, 4, NULL, NULL, NULL, false);
